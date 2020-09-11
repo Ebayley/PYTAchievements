be2cb67dc43504fbc395f7f6be242ae8bbd8466f
Ebayley <54781425+Ebayley@users.noreply.github.com>
2020-09-11 11:52:16 +0200

    changed code

--- PYTB1L2PlayerStatistics/playerStatistics.py
+++ PYTB1L2PlayerStatistics/playerStatistics.py
@@ -7,18 +7,17 @@ playerSpeed = 'slow'
 playerMagic = 'light'
 weapon = 'wand'
 equipment = 'robe'
-playerAlignment = 0
-goodAlignment = 0
+playerAlignment = True
 
 
 print('Hello my name is ' + player + ' and these are my statistics:\n' + 
 'Health: ' + str(playerHealth) + '\nStamina: ' + str(playerStamina) + '\nMana: ' + str(playerMana) + '\nStrength: ' + str(playerStrength) + '\nSpeed: ' + playerSpeed + '\n\nAnd this is my magic type: ' + playerMagic + '\nAnd this i what I carry with me: ' + weapon + ', ' + equipment)
 
-if playerAlignment == goodAlignment:
+if playerAlignment == True:
     print('And I am aligned with the forces of good')
-elif playerAlignment == 1:
+elif playerAlignment == False:
     print('And I am aligned with the forces of evil')
 else:
     print('And I am not aligned with any faction')
 
-print('\nIk heb een integer, string en een boolean')
+print('\nIk heb een integer, string en een boolean')string en een boolean')eger, string en een boolean')
