8030d808a896c7990aac2ac1ea338fc04f0408f0
Elona Bayley <28581@ma-web.nl>
2020-09-14 10:33:34 +0200

    added PYTB1L2InterestingTellMeMore

---
+++ PYTB1L2InterestingTellMeMore/stel_vragen.py
@@ -0,0 +1,5 @@
+leeftijd = input("Hoe oud ben je?\n")
+naam = input("Wat is je naam?\n")
+woonplaats = input("Waar woon je?\n")
+
+print("Hallo " + naam + ", dus je bent " + leeftijd + " jaar oud en je woont in " + woonplaats)
