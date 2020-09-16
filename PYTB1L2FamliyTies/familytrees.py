07eb3ac5ad78718edaf5d724d36fed41ac65b1cc
Elona Bayley <28581@ma-web.nl>
2020-09-16 11:20:18 +0200

    added family ties

---
+++ PYTB1L2FamliyTies/familytrees.py
@@ -0,0 +1,7 @@
+mijnNaam = 'Elona Bayley'
+leeftijd = 20
+home = 'Haarlem'
+zusNaam = 'Maren'
+broerNaam = 'Luc'
+
+print('Mijn naam is ' + mijnNaam + ', ik ben ' + str(leeftijd) + ' jaar oud en ik heb een broer en een zus.\nIk woon in ' + home + '. mijn zus heet ' + zusNaam + ' en mijn broer heet ' + broerNaam + '.')
